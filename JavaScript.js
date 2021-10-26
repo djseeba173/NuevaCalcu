cb06881bc72cdd4a3a4a2175e7ba6badc460928e
djseeba173 <djseeba173@gmail.com>
2021-10-26 19:44:16 -0300

    first comit

---
+++ JavaScript.js
@@ -0,0 +1,53 @@
+﻿const Chart = require("./Scripts/Chart");
+
+function Ejemplo() {
+    var ejemplo = document.getElementById("ejemplo");
+
+    ejemplo.value = "Ax^2 + Bx + C"
+}
+function SacarLasRaices() {
+    SacarRaiz1();
+    SacarRaiz2();
+}
+
+
+function SacarRaiz1() {
+    var A = document.getElementById('A').value;
+    var B = document.getElementById('B').value;
+    var C = document.getElementById('C').value;
+
+    var resultado = document.getElementById('resultado');
+
+    var primerCuenta = (-B) - (Math.sqrt( (B * B) - 4 * A * C));
+    
+
+    
+
+    if ((2 * A) != 0) {
+        var segundaCuenta = primerCuenta / (2 * A);
+        
+        resultado.value = segundaCuenta;
+
+    } else {
+        resultado.value = 'No hay raices disponibles';
+    }
+}
+function SacarRaiz2() {
+    var A = document.getElementById('A').value;
+    var B = document.getElementById('B').value;
+    var C = document.getElementById('C').value;
+
+    var resultado = document.getElementById('resultado2');
+
+    var primerCuenta = (-B) + (Math.sqrt((B * B) - 4 * A * C));
+    
+    if ((2 * A) != 0) {
+        var segundaCuenta = primerCuenta / (2 * A);
+        resultado.value = segundaCuenta;
+
+    } else {
+        resultado.value = 'No hay raices disponibles';
+    }
+
+
+}
