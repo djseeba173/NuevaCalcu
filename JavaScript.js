7fb016683df54e09fdc52598eded927a6df52625
djseeba173 <djseeba173@gmail.com>
2021-10-26 22:11:32 -0300

    solucion NaN

--- JavaScript.js
+++ JavaScript.js
@@ -11,6 +11,16 @@ function SacarLasRaices() {
 }
 
 
+function calcularRaizCuadrada(A, B, C) {
+    var cuentaProblematica = (B * B) - (4 * A * C);
+    var resultadoReal = 0;
+    if (cuentaProblematica > 0) {
+        resultadoReal = Math.sqrt(cuentaProblematica);
+    }
+
+    return resultadoReal;
+}
+
 function SacarRaiz1() {
     var A = document.getElementById('A').value;
     var B = document.getElementById('B').value;
@@ -18,20 +28,23 @@ function SacarRaiz1() {
 
     var resultado = document.getElementById('resultado');
 
-    var primerCuenta = (-B) - (Math.sqrt( (B * B) - 4 * A * C));
-    
+    var sqrt = calcularRaizCuadrada(A, B, C);
+    var pudo = false;
 
-    
-
-    if ((2 * A) != 0) {
-        var segundaCuenta = primerCuenta / (2 * A);
-        
-        resultado.value = segundaCuenta;
-
-    } else {
-        resultado.value = 'No hay raices disponibles';
+    if (sqrt != 0 && A != 0) {
+        var resulReal = (-B) - sqrt;
+        var resulTotal = resulReal / (2 * A);
+        pudo = true;
     }
+
+    if (pudo) {
+        resultado.value = resulTotal;
+    } else {
+        resultado.value = 'no existen raices';
+    }
+
 }
+
 function SacarRaiz2() {
     var A = document.getElementById('A').value;
     var B = document.getElementById('B').value;
@@ -39,15 +52,19 @@ function SacarRaiz2() {
 
     var resultado = document.getElementById('resultado2');
 
-    var primerCuenta = (-B) + (Math.sqrt((B * B) - 4 * A * C));
-    
-    if ((2 * A) != 0) {
-        var segundaCuenta = primerCuenta / (2 * A);
-        resultado.value = segundaCuenta;
+    var sqrt = calcularRaizCuadrada(A, B, C);
+    var pudo = false;
 
-    } else {
-        resultado.value = 'No hay raices disponibles';
+    if (sqrt != 0 && A != 0) {
+        var resulReal = (-B) + sqrt;
+        var resulTotal = resulReal / (2 * A);
+        pudo = true;
     }
 
+    if (pudo) {
+        resultado.value = resulTotal;
+    } else {
+        resultado.value = 'no existen raices';
+    }
 
 }
